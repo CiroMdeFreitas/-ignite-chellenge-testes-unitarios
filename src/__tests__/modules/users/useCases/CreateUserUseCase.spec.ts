30a60e671643fdf216d9afef9a3a448941022a20
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-05-20 15:48:40 -0300

    Create CreateUserUseCase.spec.ts

---
+++ src/__tests__/modules/users/useCases/CreateUserUseCase.spec.ts
@@ -0,0 +1,13 @@
+import { CreateUserUseCase } from '../../../../modules/users/useCases/createUser/CreateUserUseCase';
+
+let createUserUseCase: CreateUserUseCase;
+
+describe("Create User", () => {
+    it("should be able to create user", async () => {
+
+    });
+
+    it("should be able to create user if email is already in use", async () => {
+
+    });
+});

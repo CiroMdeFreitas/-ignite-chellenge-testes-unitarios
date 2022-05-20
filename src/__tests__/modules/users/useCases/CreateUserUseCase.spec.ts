ebcb62ba4599e6ae728b94ae1bf299900369850a
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-05-20 16:23:51 -0300

    Create user "should be able to create user" test

--- src/__tests__/modules/users/useCases/CreateUserUseCase.spec.ts
+++ src/__tests__/modules/users/useCases/CreateUserUseCase.spec.ts
@@ -1,10 +1,22 @@
-import { CreateUserUseCase } from '../../../../modules/users/useCases/createUser/CreateUserUseCase';
+import { InMemoryUsersRepository } from "../../../../modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "../../../../modules/users/repositories/IUsersRepository";
+import { CreateUserUseCase } from "../../../../modules/users/useCases/createUser/CreateUserUseCase";
 
 let createUserUseCase: CreateUserUseCase;
+let usersRepository: IUsersRepository;
 
 describe("Create User", () => {
-    it("should be able to create user", async () => {
+    beforeEach(async () => {
+        usersRepository = new InMemoryUsersRepository();
+        createUserUseCase = new CreateUserUseCase(usersRepository);
+    });
 
+    it("should be able to create user", async () => {
+        const tryRegisterUser = await createUserUseCase.execute({ name: "Fulano", email: "fulano@ignite.com.br", password: "123456789" });
+
+        const registeredUser = await usersRepository.findByEmail(tryRegisterUser.email);
+
+        expect(tryRegisterUser).toEqual(registeredUser);
     });
 
     it("should be able to create user if email is already in use", async () => {

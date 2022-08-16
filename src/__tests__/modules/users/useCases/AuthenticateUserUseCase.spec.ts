33b17f1c61ac356918edfeeafcdb0c3e2bf63b25
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-08-16 19:55:59 -0300

    Update AuthenticateUserUseCase.spec.ts

--- src/__tests__/modules/users/useCases/AuthenticateUserUseCase.spec.ts
+++ src/__tests__/modules/users/useCases/AuthenticateUserUseCase.spec.ts
@@ -1,19 +1,56 @@
-import { InMemoryUsersRepository } from "../../../../modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { v4 as uuidV4 } from "uuid";
+import { hash } from "bcryptjs";
+import { Connection, createConnection } from "typeorm";
 import { IUsersRepository } from "../../../../modules/users/repositories/IUsersRepository";
+import { UsersRepository } from "../../../../modules/users/repositories/UsersRepository";
 import { AuthenticateUserUseCase } from "../../../../modules/users/useCases/authenticateUser/AuthenticateUserUseCase";
 
 
 let authenticateUserUseCase: AuthenticateUserUseCase;
 let usersRepository: IUsersRepository;
+let connection: Connection;
 
-describe("Autenticate user", () => { 
 describe("Authenticate user", () => { 
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+
+        const id = uuidV4()
+        const password = await hash("admin", 8);
+        await connection.query(
+            `
+                INSERT INTO USERS(id, name, email, password, created_at, updated_at) 
+                values(
+                    '${id}',
+                    'admin',
+                    'admin@rentx.com,br',
+                    '${password}',
+                    'now()',
+                    'now()')
+            `)
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
     beforeEach(async () => {
-        usersRepository = new InMemoryUsersRepository();
+        usersRepository = new UsersRepository();
         authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository);
     });
 
-    it("should be able to login if email and password are correct", () => {});
+    it("should be able to login if email and password are correct", async () => {
+        const password = "123456789";
+        const hashedPassword = await hash("123456789", 8)
+        const registeredUser = await usersRepository.create({ name: "Fulano", email: "fulano@ignite.com.br", password: String(hashedPassword)});
+
+        const loginAttempt = await authenticateUserUseCase.execute({ email: registeredUser.email, password: password });
+        
+        expect(loginAttempt).toHaveProperty("user");
+        expect(loginAttempt).toHaveProperty("token");
+    });
 
     it("should not be able to login if email is incorrect", () => {});
 

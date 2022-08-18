bf267f4ceaeb5bd00f20d4ca3aaa38c20d6ed944
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-08-18 16:30:54 -0300

    Create CreateUserController.spec.ts

---
+++ src/__tests__/modules/users/Controllers/CreateUserController.spec.ts
@@ -0,0 +1,6 @@
+describe("Create User Controller", () => {
+
+    it("should be able to create user", async () => {});
+
+    it("should be able to create user if email is already in use", () => {});
+});

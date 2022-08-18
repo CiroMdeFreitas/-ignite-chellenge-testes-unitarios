2bc66ae95398af275d1e533008c98cd1bce5dd51
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-08-18 19:50:21 -0300

    savepoint

--- src/__tests__/modules/users/Controllers/CreateUserController.spec.ts
+++ src/__tests__/modules/users/Controllers/CreateUserController.spec.ts
@@ -1,6 +1,45 @@
+import { hash } from "bcryptjs";
+import { v4 as uuidV4 } from "uuid";
+import { Connection, createConnection } from "typeorm";
+import request from "supertest";
+import { app } from "../../../../app";
+
+let connection: Connection;
+
 describe("Create User Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
 
-    it("should be able to create user", async () => {});
+        const id = uuidV4()
+        const password = await hash("admin", 8);
+        await connection.query(
+            `
+                INSERT INTO USERS(id, name, email, password, created_at, updated_at) 
+                values(
+                    '${id}',
+                    'admin',
+                    'admin@rentx.com,br',
+                    '${password}',
+                    'now()',
+                    'now()')
+            `)
+    });
 
-    it("should be able to create user if email is already in use", () => {});
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it("should be able to create user", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Fulano",
+            email: "fulano@ignite.com.br",
+            password: "123456789"
+        });
+
+        expect(response.status).toBe(201);
+    });
+
+    it("should not be able to create user if email is already in use", () => {});
 });

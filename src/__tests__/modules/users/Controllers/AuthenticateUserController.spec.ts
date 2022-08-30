405380f75d79159c16515763f41bb6cdac1e5362
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-08-30 17:17:26 -0300

    Create AuthenticateUserController.spec.ts

---
+++ src/__tests__/modules/users/Controllers/AuthenticateUserController.spec.ts
@@ -0,0 +1,51 @@
+import { v4 as uuidV4 } from "uuid";
+import { Connection, createConnection } from "typeorm";
+import { hash } from "bcryptjs";
+
+let connection: Connection;
+
+describe("Authenticate user", () => { 
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const adminId = uuidV4()
+        const adminPassword = await hash("admin", 8);
+        await connection.query(
+            `
+                INSERT INTO USERS(id, name, email, password, created_at, updated_at) 
+                values(
+                    '${adminId}',
+                    'admin',
+                    'admin@rentx.com,br',
+                    '${adminPassword}',
+                    'now()',
+                    'now()')
+            `);
+
+        const userId = uuidV4()
+        const userPassword = await hash("123456789", 8);
+        await connection.query(
+            `
+                INSERT INTO USERS(id, name, email, password, created_at, updated_at) 
+                values(
+                    '${userId}',
+                    'Fulano',
+                    'fulano@ignite.com.br',
+                    '${userPassword}',
+                    'now()',
+                    'now()')
+            `);
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it("should be able to login if email and password are correct", async () => {});
+
+    it("should not be able to login if email is incorrect", async () => {});
+
+    it("should not be able to login if password is incorrect", async () => {});
+});

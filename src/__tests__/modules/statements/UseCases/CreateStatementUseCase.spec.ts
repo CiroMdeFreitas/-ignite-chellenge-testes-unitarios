acf8817e3b05488e771a9f2a03cd5aa80a222ad6
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-09-01 17:06:08 -0300

    Test File: Create Statement Use Case

---
+++ src/__tests__/modules/statements/UseCases/CreateStatementUseCase.spec.ts
@@ -0,0 +1,55 @@
+import { hash } from "bcryptjs";
+import { v4 as uuidV4 } from "uuid";
+import { Connection } from "typeorm";
+import createConnection from '../../../../database';
+import { UsersRepository } from "../../../../modules/users/repositories/UsersRepository";
+import { IUsersRepository } from "../../../../modules/users/repositories/IUsersRepository";
+import { IStatementsRepository } from "../../../../modules/statements/repositories/IStatementsRepository";
+import { CreateStatementUseCase } from "../../../../modules/statements/useCases/createStatement/CreateStatementUseCase";
+import { StatementsRepository } from "../../../../modules/statements/repositories/StatementsRepository";
+
+let createStamentUseCase: CreateStatementUseCase; 
+let statementsRepository: IStatementsRepository; 
+let usersRepository: IUsersRepository;
+let connection: Connection;
+
+const userId = uuidV4();
+
+describe("Create Statement Use Case", () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const userPassword = await hash("password", 8);
+        await connection.query(
+            `
+                INSERT INTO USERS(id, name, email, password, created_at, updated_at) 
+                values(
+                    '${userId}',
+                    'User',
+                    'user@email.com',
+                    '${userPassword}',
+                    'now()',
+                    'now()')
+            `);
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+    
+    beforeEach(async () => {
+        usersRepository = new UsersRepository();
+        statementsRepository = new StatementsRepository();
+        createStamentUseCase = new CreateStatementUseCase(usersRepository, statementsRepository);
+    });
+
+    it("Should be able to make a deposit on user's account", () => {});
+
+    it("Should be able to make a withdraw from user's account", () => {});
+
+    it("Should not be able to do a statement if user is invalid", () => {});
+
+    it("Should not be able to make a withdraw from user's account if user's balance is insufficient", () => {});
+});

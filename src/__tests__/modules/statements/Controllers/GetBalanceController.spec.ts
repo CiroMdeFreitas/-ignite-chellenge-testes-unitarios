ae207e191017443dc386f4a1e4b2b2c490db0eaa
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-09-01 16:11:13 -0300

    Create GetBalanceController.spec.ts

---
+++ src/__tests__/modules/statements/Controllers/GetBalanceController.spec.ts
@@ -0,0 +1,35 @@
+import { hash } from "bcryptjs";
+import { v4 as uuidV4 } from "uuid";
+import { Connection } from "typeorm";
+import createConnection from '../../../../database';
+
+let connection: Connection;
+
+const userEmail = "user@email.com";
+const userPassword = "password";
+
+describe("Show User Profile Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+        
+        const userId = uuidV4();
+        const hashedPassword = await hash(userPassword, 8);
+        await connection.query(
+            `
+                INSERT INTO USERS(id, name, email, password, created_at, updated_at) 
+                values(
+                    '${userId}',
+                    'User',
+                    '${userEmail}',
+                    '${hashedPassword}',
+                    'now()',
+                    'now()')
+            `);
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+});
